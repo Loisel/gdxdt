ef1655177e1149b0749420a4ed8a5bddc860ecbc
Alois Dirnaichner <alodi@directbox.com>
2018-12-14 10:53:07 +0100

    Add interface to write data.tables to gdx. Add test.

--- R/write.R
+++ R/write.R
@@ -2,6 +2,10 @@
 #'
 #' Save to a GAMS gdx file. Works on a named list providing
 #' domains and data as given by gdxrrw::rgdx.
+#' This is a *workaround* to fix bugs in the implementation of gdxrrw::wgdx,
+#' namely the problems that domains are lost when writing the output of gdxrrw:rgdx
+#' and that for variables, a `_field` domain has always to be given.
+#' Using this wrapper, round-tripping data between R and gdx files should be possible.
 #' @param gdx the gdx filename.
 #' @param var list of properties of a gdx symbol as provided by gdxrrw::rgdx.
 #' @export
@@ -11,12 +15,70 @@
 
 raw2gdx <- function(gdx, var){
     uels <- list()
-    ndim <- length(var$uels)
-    # variables have a field dimension
-    if(var$type == "variable")
-        ndim <- ndim - 1
-    for(n in 1:ndim){
+    if (var$type == "variable" & var$field != "all"){
+        ## create field dimension
+        flds <- c("l", "m", "lo", "s")
+        fld_idx <- match(var$field, flds)
+        ## add it the matrix
+        var$val <- cbind(var$val[, 1:var$dim], rep(fld_idx, nrow(var$val)), var$val[, var$dim + 1])
+        ## add it to domains
+        var$domains <- c(var$domains, "_field")
+        ## add it to uels
+        var$uels[[var$dim + 1]] <- flds
+        ## field to all (required to write!)
+        var$field <- "all"
+    }
+    for (n in 1:var$dim){
         uels[[n]] <- list(name=var$domains[[n]], type="set", uels=list(var$uels[[n]]))
     }
     gdxrrw::wgdx(gdx, var, uels)
+}
+
+
+#' dt2gdx
+#'
+#' Save a data.table to a GAMS gdx file.
+#' @param gdx the gdx filename.
+#' @param dt a data.table.
+#' @param name name of the variable.
+#' @param valcol name of data column.
+#' @param uelcols vector of names with dimensions.
+#' @param type type of symbol (variable or parameter)
+#' @param field the field if `type == 'variable'`
+#' @export
+#' @examples
+#' # round-trip:
+#'
+
+dt2gdx <- function(gdx, dt, name, valcol, uelcols, type, field="l"){
+    data <- list(name=name, type=type, domains=uelcols, dim=length(uelcols))
+    if(type == "variable"){
+        data[["field"]] <- field
+    }
+
+    uels <- list()
+    nuel <- 1
+
+    cols <- c(uelcols, valcol)
+    dt <- dt[, cols, with=F]
+
+    for(uel in uelcols){
+        labels <- unique(dt[[uel]])
+        uels[[nuel]] <- labels
+
+        map <- data.table(1:length(labels), labels)
+        setnames(map, "labels", uel)
+
+        dt <- dt[map, on=uel]
+        dt[, (uel) := NULL]
+        setnames(dt, "V1", uel)
+        nuel <- nuel + 1
+    }
+
+    data[["val"]] <- unname(as.matrix(dt[, cols, with=F]))
+    data[["uels"]] <- uels
+    ## type code is "free":
+    data[["typeCode"]] <- 5
+
+    raw2gdx(gdx, data)
 }
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -14,11 +14,37 @@ test_that("reading a variable works", {
     expect_gt(length(dt), 0)
 })
 
-test_that("round-trip of a variable works", {
+test_that("round-trip of a variable with all field works", {
     test_gdx <- "test.gdx"
     test_var <- "v_welfare"
     raw2gdx(test_gdx, rgdx(gdx, list(name=test_var, field="all")))
     dt <- read_variable(test_gdx, test_var, "l")
+    expect_is(dt, "data.table")
+    expect_gt(length(dt), 0)
+    file.remove(test_gdx)
+})
+
+test_that("round-trip of a variable with l field works", {
+    test_gdx <- "test.gdx"
+    test_var <- "v35_shEsPeT"
+    raw2gdx(test_gdx, rgdx(gdx, list(name=test_var)))
+    dt <- read_variable(test_gdx, test_var, "l")
+    expect_is(dt, "data.table")
+    expect_gt(length(dt), 0)
+    file.remove(test_gdx)
+})
+
+test_that("round-trip between variable and data.table works", {
+    test_gdx <- "test.gdx"
+    test_var <- "v35_shEsPeT"
+    ## read var
+    dt <- read_variable(gdx, test_var)
+    expect_is(dt, "data.table")
+    expect_gt(length(dt), 0)
+    ## write var
+    dt2gdx(test_gdx, dt, test_var, "value", c("ttot", "all_regi", "all_te"), "variable")
+    ## read var
+    dt <- read_variable(test_gdx, test_var)
     expect_is(dt, "data.table")
     expect_gt(length(dt), 0)
     file.remove(test_gdx)
